7b38c0bad6c6efc85e18b1fe9c77ea0211aba50f
abdulrhman.alsarari <abdulrhman.alsarari1@gmail.com>
2025-11-12 20:15:12 +0300

    Update ArticleServise.java

--- src/main/java/com/springbooteventsystem/managementsystem/Servise/ArticleServise.java
+++ src/main/java/com/springbooteventsystem/managementsystem/Servise/ArticleServise.java
@@ -21,4 +21,50 @@ public class ArticleServise {
         articles.add(article);
 
     }
+
+
+    public boolean update(int index , Article article){
+        if (index>0&&index<=articles.size()){
+            articles.set(index, article);
+            return true;
+        }
+        return false;
+    }
+    public boolean deleteArticle(int index ){
+        if (index>0&&index<=articles.size()){
+            articles.remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean publishArticle(int index , Article  article){
+
+        if (index<0&&index>articles.size()){
+            return true;
+
+        }
+        return false;
+    }
+    public ArrayList<Article> getPublishedArticles(){
+        for (Article ae : articles){
+            if (articles.isEmpty()){
+                articles.add(ae);
+            }
+
+        }
+        return getPublishedArticles();
+    }
+
+    public ArrayList<Article> getByCategory(String category) {
+        ArrayList<Article> result = new ArrayList<>();
+        for (Article article : articles) {
+            if (article.getCategory().equalsIgnoreCase(category)) {
+                result.add(article);
+            }
+        }
+        return result;
+    }
+
+    
 }
