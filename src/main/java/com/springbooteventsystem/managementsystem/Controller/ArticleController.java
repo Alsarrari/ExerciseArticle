0959cb70dd4a4bc430aa9b783642976bff234d53
abdulrhman.alsarari <abdulrhman.alsarari1@gmail.com>
2025-11-12 17:32:47 +0300

    Implement addArticle method in ArticleController
    Added a method to handle adding articles with validation.

--- src/main/java/com/springbooteventsystem/managementsystem/Controller/ArticleController.java
+++ src/main/java/com/springbooteventsystem/managementsystem/Controller/ArticleController.java
@@ -6,5 +6,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/a1/article")
 public class ArticleController {
+  
+    @GetMapping("/get")
+    public ResponseEntity<?> getAllArticle(){
+
+        return ResponseEntity.status(200).body("get All Article");
+    }
+
+}
+public ResponseEntity<?> addArticle(@RequestBody @Valid Article article , Errors errors ){
+if (errors.hasErrors()){
+    return ResponseEntity.status(400).body(errors);
+}
+return ResponseEntity.status(200).body("added article");
+
+
 
 }
