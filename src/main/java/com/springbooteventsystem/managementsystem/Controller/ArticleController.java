1ef113c0edae6f1c0f07862e52779ff21c0cbe4f
abdulrhman.alsarari <abdulrhman.alsarari1@gmail.com>
2025-11-12 20:16:20 +0300

    Add update, delete, publish, and category endpoints

--- src/main/java/com/springbooteventsystem/managementsystem/Controller/ArticleController.java
+++ src/main/java/com/springbooteventsystem/managementsystem/Controller/ArticleController.java
@@ -21,5 +21,36 @@ if (errors.hasErrors()){
 return ResponseEntity.status(200).body("added article");
 
 
+@PutMapping("/update/{id}")
+public ResponseEntity<?> upDate(@PathVariable int id ,@RequestBody @Valid Article article , Errors errors){
+        if (errors.hasErrors()){
+            return ResponseEntity.status(400).body(errors);
+        }
+
+        return ResponseEntity.status(200).body(new ApiResponse("update article"));
+}
+@DeleteMapping("delete/{index}")
+public ResponseEntity<?> deleteArticle(@PathVariable int index , Errors  errors ){
+        if (errors.hasErrors()){
+            return ResponseEntity.status(400).body(errors);
+        }
+        return ResponseEntity.status(200).body(new ApiResponse("delete Article "));
+    }
+    @PutMapping("/publish/{id}")
+    public boolean publishArticle(@PathVariable int id ,@RequestBody Article article){
+        if (publishArticle(id, article)){
+            return ResponseEntity.status(400).body(publishArticle(id, article));
+        }
+        return ResponseEntity.status(200).body(new ApiResponse("publishArticle"));
+    }
+    @GetMapping("/publish")
+    public ResponseEntity<?> getPublished(){
+        return ResponseEntity.status(200).body(new ApiResponse("publish"));
+    }
+    @GetMapping("/category")
+        public ResponseEntity<?> getByCategory(@PathVariable String category){
+return ResponseEntity.status(200).body(getByCategory(category));
+        }
+
 
 }
